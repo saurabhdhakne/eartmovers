cca7b8d243f3d31cbf2e67b158ba3af9ead46e12
saurabhdhakne <sbdhake309@gmail.com>
2021-11-25 00:17:19 +0530

    first commit

---
+++ assets/js/navitem.js
@@ -0,0 +1,14 @@
+$(function () { 
+    var header = $(".onscrolldark"); 
+    $(window).scroll(function () { 
+        var scroll = $(window).scrollTop(); 
+
+        if (scroll >= 100) { 
+            header.removeClass('text-light') 
+                    .addClass("text-dark"); 
+        } else { 
+            header.removeClass("text-dark") 
+                    .addClass('text-light'); 
+        } 
+    }); 
+}); 
